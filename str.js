6072d3c29891074db3eb5db2f7892e13c8cb8f92
talarisulochana <talarisulochana123@gmail.com>
2024-11-12 15:29:23 +0530

    Add files via upload

---
+++ str.js
@@ -0,0 +1,25 @@
+//1.let str = "Hello World!";Relace the substring “World!” with “My Dear Friend” ..Write the program for that
+let str = "Hello World!";
+let updated= str.replace("World!","My Dear Friend");
+console.log(updated)
+
+//2.let a=”Hungry”;Repeat the “Hungry” for 10 timesWrite the program for that
+let a="Hungry ".repeat(10);
+console.log(a)
+
+//3.let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";Find the last index of the string “fox”
+let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+console.log(sentence.lastIndexOf("fox"))
+
+//4.let message = "Hello, world!";Find the string starts with “hi”Write code for that
+let message = "Hello, world!";
+let A =message.startsWith("hi")
+console.log(A)
+
+//5.let fileName = "document.pdf";Find the string ends with “.pdf”Write code for that
+
+let fileName ="document.pdf";
+let P=fileName.endsWith(".pdf")
+console.log(P)
+
+
